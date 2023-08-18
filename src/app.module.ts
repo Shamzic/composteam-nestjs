7d3a5fbcab892d78fb9c25d4506da6f1532a86cf
Shamzic <shamery@free.fr>
2023-08-19 01:26:04 +0200

    config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.dev',
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev',
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
